70d2522ecb66449d87dcfc126db4d97d7957a3bd
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-19 09:15:10 -0300

    Refactoring routes to serve better the client

--- src/routes.js
+++ src/routes.js
@@ -14,13 +14,11 @@ const jwtCheck = expressJwt({
 routes.post('/login', UserController.login);
 
 routes.get('/resource', (req, res) => {
-  res.status(200).json({ success: 'Public resouse, you can see this' });
+  res.status(200).send('Public resource, you can see this');
 });
 
 routes.get('/resource/secret', jwtCheck, (req, res) => {
-  res
-    .status(200)
-    .json({ success: 'Secret resouse, you should be loggedin to see this' });
+  res.status(200).send('Secret resource, you should be logged in to see this');
 });
 
 routes.get('*', (req, res) => {
