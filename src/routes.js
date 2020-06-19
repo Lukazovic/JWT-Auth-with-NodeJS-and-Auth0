13edd1d224ebfc1681ad0a0d5313ec6764d70f6c
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-19 00:26:22 -0300

    Add jwtCheck middleware to the private route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const expressJwt = require('express-jwt');
 
 const app = require('./app');
 
@@ -6,13 +7,17 @@ const UserController = require('./app/controllers/UserController');
 
 const routes = new Router();
 
+const jwtCheck = expressJwt({
+  secret: 'secretkey',
+});
+
 routes.post('/login', UserController.login);
 
 routes.get('/resource', (req, res) => {
   res.status(200).json({ success: 'Public resouse, you can see this' });
 });
 
-routes.get('/resource/secret', (req, res) => {
+routes.get('/resource/secret', jwtCheck, (req, res) => {
   res
     .status(200)
     .json({ success: 'Secret resouse, you should be loggedin to see this' });
