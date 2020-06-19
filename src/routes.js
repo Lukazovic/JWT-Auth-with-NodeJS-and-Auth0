13a8c8a0b05431ba180d35459a301f957772c756
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-19 00:23:11 -0300

    Refactoring routes to add public & private routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+
 const app = require('./app');
 
 const UserController = require('./app/controllers/UserController');
@@ -7,10 +8,14 @@ const routes = new Router();
 
 routes.post('/login', UserController.login);
 
-routes.get('/status', (req, res) => {
-  const localTime = new Date().toLocaleTimeString();
+routes.get('/resource', (req, res) => {
+  res.status(200).json({ success: 'Public resouse, you can see this' });
+});
 
-  res.status(200).send(`Server time is ${localTime}`);
+routes.get('/resource/secret', (req, res) => {
+  res
+    .status(200)
+    .json({ success: 'Secret resouse, you should be loggedin to see this' });
 });
 
 routes.get('*', (req, res) => {
